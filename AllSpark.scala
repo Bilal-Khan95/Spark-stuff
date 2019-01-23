dd4e165d5a2cb0f47d62450854729a49496da156
Bilal Khan <mohammedbilalkhan95@gmail.com>
2019-01-23 17:40:21 +0000

    updated spark code

--- AllSpark.scala
+++ AllSpark.scala
@@ -10,15 +10,23 @@ object AllSpark{
     val conf = new SparkConf().setMaster("local[*]").setAppName("main")
     val sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val rdd1 = sc.textFile("C:\\Users\\Admin\\Documents\\Bilals Stuff\\Bilal - Scala\\data.txt")
-    val topLine = rdd1.first()
-    val rdd2 = rdd1.filter(x => x!= topLine)
-    val rdd3 = rdd2.map(x => {
-      val rec = x.split(", ")
-      (rec(0).toInt, rec(1), rec(2), rec(3).toInt, rec(4))
-    })
-    val df = sqlContext.createDataFrame(rdd3).toDF("REGNO","NAME","SUBJECT","MARKS","CLIENT")
-    df.show()
+//    val rdd1 = sc.textFile("C:\\Users\\Admin\\Documents\\Bilals Stuff\\Bilal - Scala\\data.txt")
+//    val topLine = rdd1.first()
+//    val rdd2 = rdd1.filter(x => x!= topLine)
+//    val rdd3 = rdd2.map(x => {
+//      val rec = x.split(", ")
+//      (rec(0).toInt, rec(1), rec(2), rec(3).toInt, rec(4))
+//    })
+//    val df = sqlContext.createDataFrame(rdd3).toDF("REGNO","NAME","SUBJECT","MARKS","CLIENT")
+//    df.show()
 //    df.select(df("NAME") === "Shafeeq").show
+
+    val rdd1 = sc.textFile("C:\\Users\\Admin\\Downloads\\movies.txt")
+    val rdd2 = rdd1.map(x => {
+      val rec = x.split("\t")
+      (rec(0).toInt,rec(1).toInt,rec(2),rec(3).toInt)
+    })
+    val movieDF = sqlContext.createDataFrame(rdd2).toDF("USER ID", "MOVIE ID", "RATING", "TIMESTAMP")
+    movieDF.filter(movieDF("RATING") = 5)
   }
 }
